16978716ca764b850cf39b61f4ac7c7637d11b0c
unknown <youssefhoussein701@gmail.com>
2023-08-13 01:38:12 +0300

    <BE> added student middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,7 +8,9 @@ use App\Http\Controllers\AuthController;
 Route::group(["middleware" => "admin", "prefix" => "admin"], function(){
     Route::post('create-account', [AuthController::class, 'createAccount']);
 });
-
+Route::group(["middleware"=>"student","prefix"=>"student"], function(){
+    
+});
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
