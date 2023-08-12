c6282ddea2e1d085a51e683d4fafb75d439c2b6f
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-13 02:49:59 +0300

    <BE> Edited the function that  get students course progress

--- backend/app/Http/Controllers/ParentController.php
+++ backend/app/Http/Controllers/ParentController.php
@@ -8,21 +8,27 @@ use App\Models\Grade;
 
 class ParentController extends Controller
 {
-    public function getStudentProgress($parent,$student_name,$course_name){
+    public function getStudentProgress($parent_name,$student_name,$course_name){
         $full_name= explode(" ", $student_name);
         $first_name=$full_name[0];
         $last_name=$full_name[1];
-        $student= User::where("first_name",$first_name)->first();
-        $total_grades =$student->grades()->get()->pluck('grade');
-        $total_submissions=$student->submission()->get()->count();
-
-        return response()->json([
-            'parent'=>$parent,
-            'student_fname:'=>$first_name,
-            'student_lname:'=>$last_name,
-            'course:'=>$course_name,
-            'total grades'=>$total_grades,
-            'total-submissions'=>$total_submissions
-        ]);
+        $student= User::where("first_name",$first_name)->where("last_name",$last_name)->first();
+        $parent=User::where("first_name",$parent_name)->first();
+        $parent_id=$student->parents->where("id",$parent->id)->first()->id;
+        if($parent->id===$parent_id){
+            $total_grades =$student->grades()->get()->pluck('grade');
+                $total_submissions=$student->submission()->get()->count();
+                return response()->json([
+                    'parent'=>$parent_name,
+                    'student_fname:'=>$first_name,
+                    'student_lname:'=>$last_name,
+                    'course:'=>$course_name,
+                    'total grades'=>$total_grades,
+                    'total-submissions'=>$total_submissions
+                ]);
+        }    
+        else{
+            return response()->json([
+                'result'=>"An error has Occured"]);}
     }
 }
