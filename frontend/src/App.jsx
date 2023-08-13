c9d4d8fb81912bcaa0add69176ea4099292b1bee
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-14 00:01:51 +0300

    <FE> added error 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,28 @@
-import './styles/App.css';
-import './styles/colors.css';
-import './styles/utilities.css';
-import './styles/text.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AuthorizationPage from './Pages/Common/Authorization';
-import AdminLandingPage from './Pages/Admin/Landing';
-import ParentLandingPage from './Pages/Parent/Landing';
-import StudentLandingPage from './Pages/Student/Landing';
-import TeacherLandingPage from './Pages/Teacher/Landing';
+import "./styles/App.css";
+import "./styles/colors.css";
+import "./styles/utilities.css";
+import "./styles/text.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthorizationPage from "./Pages/Common/Authorization";
+import AdminLandingPage from "./Pages/Admin/Landing";
+import ParentLandingPage from "./Pages/Parent/Landing";
+import StudentLandingPage from "./Pages/Student/Landing";
+import TeacherLandingPage from "./Pages/Teacher/Landing";
+import E404 from "./Pages/E404";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthorizationPage />}/>
-        <Route path='/admin' element={<AdminLandingPage />}/>
-        <Route path='/teacher' element={<TeacherLandingPage />}/>
-        <Route path='/student' element={<StudentLandingPage />}/>
-        <Route path='/parent' element={<ParentLandingPage />}/>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<AuthorizationPage />} />
+				<Route path="/admin" element={<AdminLandingPage />} />
+				<Route path="/teacher" element={<TeacherLandingPage />} />
+				<Route path="/student" element={<StudentLandingPage />} />
+				<Route path="/parent" element={<ParentLandingPage />} />
+				<Route path="*" element={<E404 />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
