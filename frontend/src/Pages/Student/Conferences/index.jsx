4b2dc93d260723867aec0996f922b6571ad385c6
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-15 02:44:34 +0300

    <FE> Added design of student's 1-on-1 conference component

--- frontend/src/Pages/Student/Conferences/index.jsx
+++ frontend/src/Pages/Student/Conferences/index.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import Sidebar from "../../../Components/Common/Sidebar";
-
-function StudentConferencesPage() {
+import Conference from "../../../Components/Student/Conference";
+import Button from "../../../Components/Base/Button";
+const StudentConferencesPage=()=>{
+	const meeting={"date": "2022-11-10",
+					"teacher": "John Smith",
+					"link": "https://meet.google.com/abc123xyz"
+				}
 	return (
 		<div className="page flex">
 			<Sidebar
 				items={["My Courses", "Browse", "Messages", "Conferences"]}
 				selected={"Conferences"}
 			/>
-			<div className="container">Conferences</div>
+			{/* <div className="container">Conferences</div> */}
+			<div className="course-browse-container">
+			<div class="flex column meeting-container white-bg rounded ">
+				<div className="new-conference flex row center">
+					<Button 
+					color={"primary-bg"}
+					textColor={"dark-gray"}
+					text={"Book Conference"}            
+					/>
+				</div>
+				<div className="spacer-"></div>
+				<div className="spacer-30"></div>
+				<Conference meeting={meeting} />
+				<Conference meeting={meeting} />
+				<Conference meeting={meeting} />
+				<Conference meeting={meeting} />
+			</div>
+			</div>
+
 		</div>
 	);
 }
