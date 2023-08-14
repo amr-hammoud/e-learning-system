aeee5e94117a04e83c1ffc2aa599cebd488447cb
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-15 01:34:49 +0300

    <FE> Created design of course classwork component

--- frontend/src/Pages/Student/Stream/index.jsx
+++ frontend/src/Pages/Student/Stream/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import './style.css';
 import CourseStream from "../../../Components/Student/CourseStream";
+import Classwork from "../../../Components/Student/Classwork";
 
 const StudentStream=({course})=>{
     const ass={"title": "AI Assignment",
                 "description": "Implement a sentiment analysis model using a neural network architecture. The model should be trained on a dataset of movie reviews and be able to classify the sentiment of a given review as positive or negative.",
                 "submissionDate": "2022-10-15",
                 "dueDate": "2022-10-30",
+                "dueTime":"11:59",
+                "grade":"100",
                 "attachment": "https://example.com/ai_assignment.pdf"
     }
 
@@ -17,12 +20,16 @@ const StudentStream=({course})=>{
                <span>{course.title}</span>
             </div>
             <div className="spacer-30"></div>
-            <div className="stream-container flex column">
+            {/* <div className="stream-container flex column">
                 <CourseStream ass={ass}/>
                 <CourseStream ass={ass}/>
                 <CourseStream ass={ass}/>
                 <CourseStream ass={ass}/>
                 <CourseStream ass={ass}/>
+            </div> */}
+
+            <div className="flex center primary-">
+                <Classwork ass={ass}/>
             </div>
             
         </div>
