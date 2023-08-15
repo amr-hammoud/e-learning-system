55e1bf6daed7b1596c76311a77910502c6f5ded7
Omar-Arja <omaralarja17@gmail.com>
2023-08-15 05:03:35 +0300

    <FE> created login page based on user role

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./styles/colors.css";
 import "./styles/utilities.css";
 import "./styles/text.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import AuthorizationPage from "./Pages/Common/Authorization";
 import AdminLandingPage from "./Pages/Admin/Landing";
 import ParentLandingPage from "./Pages/Parent/Landing";
@@ -13,10 +14,18 @@ import TeacherMessagesPage from "./Pages/Teacher/Messages";
 import TeacherConferencesPage from "./Pages/Teacher/Conferences";
 
 function App() {
+
+	const [user, setUser] = useState({
+		first_name: "",
+		last_name: "",
+		email: "",
+		role: "",
+	});
+
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<AuthorizationPage />} />
+				<Route path="/" element={<AuthorizationPage user={user} setUser={setUser} />} />
 				<Route path="/admin" element={<AdminLandingPage />} />
 				<Route path="/teacher/courses" element={<TeacherCoursesPage />} />
 				<Route path="/teacher/messages" element={<TeacherMessagesPage />} />
