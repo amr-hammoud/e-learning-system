04981e7567bb2099877d9ad8b6e23edfebdf41a8
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-15 12:35:36 +0300

    <FE> Added course notification component

--- frontend/src/Pages/Student/Stream/index.jsx
+++ frontend/src/Pages/Student/Stream/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 import CourseStream from "../../../Components/Student/CourseStream";
 import Classwork from "../../../Components/Student/Classwork";
 import Session from "../../../Components/Student/Session";
-
+import Notification from "../../../Components/Student/Notification";
 const StudentStream=({course})=>{
     const ass={"title": "AI Assignment",
                 "description": "Implement a sentiment analysis model using a neural network architecture. The model should be trained on a dataset of movie reviews and be able to classify the sentiment of a given review as positive or negative.",
@@ -17,6 +17,12 @@ const StudentStream=({course})=>{
         "date":"2022-10-30",
         "time":"10:00",
         "link":"https://meet.google.com/abc123xyz"
+    }
+
+    const notification={
+        "date":"2022-10-30",
+        "time":"10:00",
+        "content":"A new Airbnb workshop will be held today"
     }
 	return (
     <div class="course-stream-container primary-bg">
@@ -36,9 +42,11 @@ const StudentStream=({course})=>{
             {/* <div className="flex center primary-">
                 <Classwork ass={ass}/>
             </div> */}
-            <Session session={session}/>
-            <Session session={session}/>
-            
+
+
+            {/* <Session session={session}/>
+            <Session session={session}/> */}
+            {/* <Notification notification={notification} /> */}
         </div>
     </div>
     );
