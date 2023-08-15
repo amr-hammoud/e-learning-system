b16d306829f257121c7113900c6a6a1782503da7
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-15 21:06:18 +0300

    <FE> Fetched the enrolled courses --axios

--- frontend/src/Components/Student/EnrolledCourseCard/index.jsx
+++ frontend/src/Components/Student/EnrolledCourseCard/index.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import "./style.css";
 
 const CourseEnrolledCard=({course})=>{
+    console.log(course)
 	return (
     <div class="flex column course-card white-bg rounded ">
         <div className="course-info flex column">
             <div class="course-title flex column roundedSmall">
                 <h2 className="white-color">
-                {course.title}
+                {course.name}
                 </h2>
             </div>
             <div class="course-subject">
-                <p className="dark-gray">50% Completeion</p>
+                <p className="dark-gray">{course.subject}</p>
             </div>
+            {/* <div class="course-subject">
+                <p className="dark-gray">50% Completeion</p>
+            </div> */}
     
         </div>
     </div>
